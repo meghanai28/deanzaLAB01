46021189447bf9fde8bd5c554b5d6501ad87768f
meghanai28 <66529359+meghanai28@users.noreply.github.com>
2022-08-11 15:29:16 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,19 +1,64 @@
-public class Test {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+/**
+ * Lab 01
+ * @author Lab Group 7: Meghana Indukuri & Joseph Khamisy
+ * This program implements recursive and iterative algorithms for checking if an array contains prime numbers.
+ * July 6th 2022
+ */
+public class ArrayPrimeCheck {
+    public static void main(String[] args) throws IOException
+    {
+        Scanner readSize = new Scanner(System.in);
+        int sortMaxSize = 16;
+        BufferedReader br = new  BufferedReader(new InputStreamReader(System.in)); // in order to read the entire line
+        System.out.println("Enter the array size that is above 0 and not greater than " + sortMaxSize + ":");
+        int sizeArray = readSize.nextInt();
+        if (sizeArray > sortMaxSize) {
+            sizeArray = sortMaxSize;
+        }
+        int[] inputNums = new int[sizeArray];
 
-	public static void main (String [] args)
-	{
-		
-		
-		
-	}
+        System.out.println("Please enter the " + sizeArray + " numbers separated by a space:");
+        String[] strArr;
+        strArr = br.readLine().split(" "); // read user input till end of line
+        for (int i=0; i<sizeArray; i++)
+        {
+            inputNums[i] = Integer.parseInt(strArr[i]);
+        }
+        System.out.println();
+
+        // test
+        System.out.println("Running iteration");
+        if (isArrayPrimeIter(inputNums, sizeArray)) {
+            System.out.println("Prime Array using iteration");
+        } else {
+            System.out.println("Not a Prime Array using iteration");
+        }
+        System.out.println();
+        System.out.println("Running recursion");
+
+        if (isArrayPrimeRecur(inputNums, sizeArray)) {
+            System.out.println("Prime Array using recursion");
+        } else {
+            System.out.println("Not a Prime Array using recursion");
+        }
 	
+        System.out.println("\nEnter any value to exit: ");
+        String randEnter = br.readLine();
+        readSize.close();
+    }
+    
+    
 	/*
-	 * Algorithm IsArrayPrimeIter (array, n)
+	 * Algorithm isArrayPrimeIter (array, n)
 	 * 		
-	 * 		This algorithm checks if an array contains all prime numbers or not. 
+	 * 		This algorithm efficently checks if an array contains all prime numbers or not using iteration. 
 	 * 		Pre : array - an array of integers
 	 * 			  n - the true size of array
-	 * 		Post:
+	 * 		Post: Printed statments indicating when the method is entered and exited.
 	 * 		Return: true or false
 	 * 
 	 * 		i=0
@@ -24,7 +69,7 @@ public class Test {
 	 * 			if (array[i] % 2 equals 0)
 	 * 				return false
 	 * 			end if
-	 * 			int j=3
+	 * 			j=3
 	 * 			loop(j <= sqrt(array[i]))
 	 * 				
 	 * 				if(array[i]% j equals 0)
@@ -36,78 +81,76 @@ public class Test {
 	 * 		end loop
 	 * 		return true
 	 * 
-	 * end IsArrayPrimeIter
+	 * end isArrayPrimeIter
 	 */
-	public static boolean IsArrayPrimeIter(int [] arr, int size)
-	{
-		
-		for(int i =0; i<size; i++)  // iterate through each element in loop
-		{
-			if(arr[i]<=3) // if the number is 2 or 1 or 3, return true if number is 2 or 3, and false if number is 1.
-			{
-				return !(arr[i]==1);
-			}
-			
-			if( arr[i] % 2 == 0) // if number is divisible by 2, that means the number is not prime and thus we return false. else we only have to check if arr[i] is divisible by odd numbers.
-			{
-				return false;
-			}
-			for(int j=3; j<=(Math.sqrt(arr[i])+1); j+=2) // since we only enter this loop if arr[i] is not divisble by 2, we can iterate through odd numbers.
-      // we only have to go up to the sqrt of arr[i], since if a number is prime, atleast one divisor will be less than sqrt(arr[i]).
-			{
-				if(arr[i]%j == 0) // check if number is divisible by using modulo
-				{
-					return false; 
-				}
-			}
-		}
-		return true; // if the for loops all exit without any returns, return true.
-	}
-	
-	/*
-	 * Algorithm IsArrayPrimeRecur (array, n)
+    public static boolean isArrayPrimeIter(int[] arr, int size)
+    {
+        System.out.println("Entering isArrayPrimeIter");
+        for (int i = 0; i < size; i++)
+        {
+            if(arr[i]<=3)
+            {
+                System.out.println("Leaving isArrayPrimeIter");
+                return !(arr[i]==1);
+            }
+
+            for (int j = 3; j <= Math.sqrt(arr[i])+1; j+=2)
+            {
+                if (arr[i] % j == 0)
+                {
+                    System.out.println("Leaving isArrayPrimeIter");
+                    return false;
+                }
+            }
+        }
+        System.out.println("Leaving isArrayPrimeIter");
+        return true;
+    }
+
+    /*
+	 * Algorithm isArrayPrimeRecur (array, n)
 	 * 		
 	 * 		This algorithm checks recursively if an array contains all prime numbers or not. 
 	 * 		Pre : array - an array of integers
-	 * 			  n - the true size of array
-	 * 		Post:
+	 * 			  n - the true size of array which must be greater than 0
+	 * 		Post: Printed statments indicating when the method is entered and exited.
 	 * 		Return: true or false
-	 * 
-	 * 		if(n equals 0)
-	 * 			return true
-	 * 		end if
+	 *
 	 * 		if(array[n-1] is not prime) // helper method to check if value in array is prime
 	 * 			return false
 	 * 		end if
+	 * 		if(n equals 1)
+	 * 			return true
+	 * 		end if
 	 * 		return isArrayPrimeRecur (array, n-1)
 	 * 		
-	 * end IsArrayPrimeRecur
+	 * end isArrayPrimeRecur
 	 */
-	public static boolean IsArrayPrimeRecur(int[] arr, int size)
-	{
-		
-		if(size == 0) // if the size is 0, that means we found that all numbers are prime. (i.e: this is base case)
-		{
-			return true;
-		}
-		
-		if(!isPrimeRecur(arr[size-1], 3)) // check if number is a prime number, by using helper method PrimeRecur. (divisor starts at 3 so we can optmize our code.)
-		{
-			return false;
-		}
-		
-		return IsArrayPrimeRecur(arr, size-1); // recursively call our method again, with size -1, so that we can get the next value in the array.
-		
-		
-	}
-	
-	/*
-	 * Algorithm IsPrimeRecur (number, divisor)
+    public static boolean isArrayPrimeRecur(int[] arr, int size)
+    {
+        System.out.println("Entering isArrayPrimeRecur");
+        if (!isPrimeRecur(arr[size-1], 3)) 
+        {
+            System.out.println("Leaving isArrayPrimeRecur");
+            return false;
+        }
+        if (size == 1)
+        {
+            System.out.println("Leaving isArrayPrimeRecur");
+            return true;
+        }
+        
+        System.out.println("Leaving isArrayPrimeRecur");
+        return isArrayPrimeRecur(arr, size-1);
+    }
+    
+    /*
+	 * Algorithm isPrimeRecur (number, divisor)
 	 * 		
-	 * 		This algorithm checks recursively if a number is prime or not. 
+	 * 		This algorithm efficently checks if a number is prime or not using recursion. 
 	 * 		Pre : number - integer to be checked
-	 * 		 	  divisor - integer to check if is factor of number
-	 * 		Post:
+	 * 		      divisor - integer to check if is factor of number (must be the value 3 for this method to work correctly)
+	 * 		Post: Printed statments indicating when the method is entered and exited.
 	 * 		Return: true or false
 	 * 
 	 * 		if(number <=3)
@@ -121,28 +164,27 @@ public class Test {
 	 * 		end if
 	 * 		return isPrimeRecur(num, divisor + 2)
 	 * 
-	 * end IsPrimeRecur
+	 * end isPrimeRecur
 	 */
-	private static boolean isPrimeRecur (int num, int divisor)
-	{
-		
-		if(num<=3) // if the number is 2 or 1 or 3, return true if number is 2 or 3, and false if number is 1.
-		{
-			return !(num==1);
-		}
-		
-		if(num%divisor == 0 || num%2 == 0 ) // check is number is divisible by divisor or 2. If number is divisible by 2, we return false. if it isn't we only have check
-    // if the value is divisble by odd numbers. 
-		{
-			return false;
-		}
-		
-		if(divisor > Math.sqrt(num)+1) // if number the divisor is incremented to the point it is greater than Math.sqrt(num) that means the number is a prime number. so we return true.
-		{
-			return true;
-		}
-		
-		return isPrimeRecur(num, divisor +2); // recursively call our method, with the divisor +2, since we only reach this step if the number is not divisble by 2, which means
-    // we only have to check if number is divisble by odd numbers.
-	}
+    public static boolean isPrimeRecur(int checkNum, int divisorNum)
+    {
+        System.out.println("Entering isPrimeRecur");
+        if(checkNum<=3)
+        {
+            System.out.println("Leaving isPrimeRecur");
+            return !(checkNum==1);
+        }
+        if(checkNum%divisorNum == 0 || checkNum%2 == 0 )
+        {
+            System.out.println("Leaving isPrimeRecur");
+            return false;
+        }
+        if(divisorNum > Math.sqrt(checkNum))
+        {
+            System.out.println("Leaving isPrimeRecur");
+            return true;
+        }
+        System.out.println("Leaving isPrimeRecur");
+        return isPrimeRecur(checkNum, divisorNum+2);
+    }
 }
